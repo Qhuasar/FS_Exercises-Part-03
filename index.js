8440e241b2094109e9f4767bf5e4527ad760c2ef
Pedro Matos <pedroddematos@gmail.com>
2023-03-18 13:37:54 +0000

    uibuild

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const dotenv = require("dotenv").config();
 const morgan = require("morgan");
+const cors = require("cors")
 
 const app = express();
 
 app.use(express.json());
-
+app.use(cors());
 app.use(
   morgan(":method :url :status :res[content-length] - :response-time ms", {
     skip: (req, res) => req.method === "POST",
