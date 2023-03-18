e67c451a262e69fb42e4f4b0abcf1422a661f7a9
Pedro Matos <pedroddematos@gmail.com>
2023-03-18 21:37:54 +0000

    uibuild

--- index.js
+++ index.js
@@ -25,29 +25,6 @@ app.use(
   )
 );
 
-let persons = [
-  {
-    id: 1,
-    name: "Arto Hellas",
-    number: "040-123456",
-  },
-  {
-    id: 2,
-    name: "Ada Lovelace",
-    number: "39-44-5323523",
-  },
-  {
-    id: 3,
-    name: "Dan Abramov",
-    number: "12-43-234345",
-  },
-  {
-    id: 4,
-    name: "Mary Poppendieck",
-    number: "39-23-6423122",
-  },
-];
-
 app.get("/", (req, res) => {
   res.send("<h1>PhoneBook API</h1>");
 });
@@ -95,7 +72,7 @@ app.put("/api/persons/:id", (req, res) => {
   const id = req.params.id;
   const body = req.body;
   if (mongoose.isValidObjectId(id)) {
-    Person.findByIdAndUpdate(id, { number: req.body.number }, {new: true})
+    Person.findByIdAndUpdate(id, { number: req.body.number }, { new: true })
       .exec()
       .then((data) => {
         console.log(data);
